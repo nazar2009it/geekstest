7633897b30a88e41f1a6e0734f683c7e29b9a8ac
Назар <ratbekovnazar@gmail.com>
2025-12-06 19:49:17 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// 1 задача
+// const containsOnlyDigits = (str) => {
+//     const regExp = /^\d+$/; 
+//     return regExp.test(str);
+// };
+// console.log(containsOnlyDigits("12345")); 
+// console.log(containsOnlyDigits("12a45")); 
+// 1 задача
+
+// 2 задача
+// setInterval(() => {
+//     console.log("Прошла 1 секунда");
+// }, 1000);
+// 2 задача
+
+// 3 задача
+// let i = 1;
+// const interval = setInterval(() => {
+//     console.log(i);
+//     i++;
+
+//     if (i > 10) {
+//         clearInterval(interval);
+//     }
+// }, 1000);
+// 3 задача
+
+// 4 задача
+// const box = document.querySelector('.box');
+// box.addEventListener('click', () => {
+//     box.classList.toggle('active');
+// });
+// 4 задача
+
+// 5 задача
+// function getJSON() {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', '../data/data.json');
+//     xhr.responseType = 'json';
+//     xhr.send();
+//     xhr.onload = () => {
+//         console.log(xhr.response);
+//     };
+// }
+// getJSON();
+// 5 задача
